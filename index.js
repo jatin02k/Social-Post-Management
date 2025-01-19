493090f8088d5009cd34432bc008eda6a7176c43
jatin02k <jatin02kr@gmail.com>
2025-01-19 22:12:55 +0530

    Initial commit for Social Post Management

---
+++ index.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const userModel = require("./model/user");
+const postModel = require("./model/post");
+const { name } = require("ejs");
+const post = require("./model/post");
+const crypto=require("crypto")
+const path = require("path")
+const upload=require("./config/multerconfig");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")))
+app.use(cookieParser());
+
+
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/test", (req, res) => {
+  res.render("test");
+});
+app.get("/upload",(req, res) => {
+  res.render("test");
+});
+app.post("/upload",isLoggedIn,upload.single("image"),async(req, res) => {
+  let user = await userModel.findOne({email:req.user.email});
+  user.profilepic=req.file.filename
+  await user.save()
+  res.redirect("/profile")
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/upload", (req, res) => {
+  res.render("test");
+});
+//setting up working post on profile
+app.get("/profile", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email }).populate("posts");
+  res.render("profile",{user});
+});
+
+app.get("/like/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  if(post.likes.indexOf(req.user.userid)=== -1){
+    post.likes.push(req.user.userid)
+  }
+  else{
+    post.likes.splice(post.likes.indexOf(req.user.userid),1);
+  }
+  await post.save()
+  res.redirect("/profile");
+});
+app.get("/edit/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOne({ _id: req.params.id }).populate("user");
+  res.render("edit",{post});
+});
+app.post("/update/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.findOneAndUpdate({_id: req.params.id},{content: req.body.content});
+  res.redirect("/profile");
+});
+app.get("/delete/:id", isLoggedIn, async (req, res) => {
+  let post = await postModel.deleteOne({_id: req.params.id});
+  res.redirect("/profile");
+});
+//to send user on profile
+app.post("/post", isLoggedIn, async (req, res) => {
+  let user = await userModel.findOne({ email: req.user.email });
+  let {content}=req.body;
+
+  let post = await postModel.create({
+    user:user._id,
+    content
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile")
+});
+app.get("/logout", (req, res) => {
+  res.cookie("token", "");
+  res.redirect("/login");
+});
+//this route registers to user by genreating his detials and password in mongoose
+app.post("/register", async (req, res) => {
+  let { name, username, email, age, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (user) return res.status(500).redirect("/profile");
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = await userModel.create({
+        name,
+        username,
+        email,
+        age,
+        password: hash,
+      });
+      let token = jwt.sign({ email: email, userid: user._id }, "secret");
+      res.cookie("token", token);
+      res.redirect("/login");
+    });
+  });
+});
+app.post("/login", async (req, res) => {
+  let { username, email, password } = req.body;
+  let user = await userModel.findOne({ email });
+  if (!user) return res.status(500).send("user not registered");
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: email, userid: user._id }, "secret");
+      res.cookie("token", token);
+      res.status(200).redirect("/profile");
+    } else return res.redirect("/login");
+  });
+});
+function isLoggedIn(req, res, next) {
+  if (req.cookies.token === "") res.redirect("/login");
+  else {
+    let data = jwt.verify(req.cookies.token, "secret");
+    req.user = data;
+    next();
+  }
+}
+app.listen(3000);
